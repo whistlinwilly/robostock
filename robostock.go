ed95828501a3bebc4a58ea605c983ac7d43d2fee
William Westlin <williamawestlin@gmail.com>
2016-10-26 04:55:02 +0000

    Robostock cli skeleton

--- robostock.go
+++ robostock.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"github.com/urfave/cli"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	app := cli.NewApp()
+	app.Commands = []cli.Command{
+		{
+			Name:    "fetch",
+			Aliases: []string{"f"},
+			Usage:   "Fetch and output a single sample training datapoint",
+			Action: func(c *cli.Context) error {
+				fmt.Println("Training!")
+				return nil
+			},
+		},
+	}
+       app.Name = "robostock"
+       app.Usage = "Pick stocks with robostock!"
+	app.Run(os.Args)
 }
